70e9747fae71ec081eef23a6b4786634b0bec5e6
Varad Nikharage <78202186+varad8@users.noreply.github.com>
2024-05-31 11:55:18 +0530

    Update config.py

--- config.py
+++ config.py
@@ -37,7 +37,7 @@ ADMINS = int(environ.get("ADMINS", "1812010571"))
 # Database Information
 CLONE_DB_URI = environ.get("CLONE_DB_URI", "")
 CDB_NAME = environ.get("CDB_NAME", "clonevjbotz")
-DB_URI = environ.get("DB_URI", "")
+DB_URI = environ.get("DB_URI", "mongodb+srv://varadnikharage201:xhiHpfvP7Z7s3Ove@movieastrondata.f8zrb2y.mongodb.net/?retryWrites=true&w=majority&appName=movieastrondata")
 DB_NAME = environ.get("DB_NAME", "vjbotz")
 
 # Don't Remove Credit Tg - @VJ_Botz
